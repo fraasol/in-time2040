19ee48cee4a98f72527f0fe392cc037db865f4ed
Francesco Solinas <francescosolinass@gmail.com>
2026-06-01 14:17:18 +0200

    fix base for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/in-time2040">
+    <BrowserRouter  basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
